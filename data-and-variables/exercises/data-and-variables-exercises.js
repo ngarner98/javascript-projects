c5dabbecdc51c65dd91df4e9ff005b326d868070
Nathan Garner <ngarner98.exmachina091098@gmail.com>
2023-09-10 23:16:01 -0500

    declared variables and calculated trip to mars and moon

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -1,11 +1,24 @@
 // Declare and assign the variables below
-
+let shuttle = "Determination"
+let speedMph = 17500
+let dMars = 225000000
+let dMoon = 384400
+const mpk = 0.621
 // Use console.log to print the 'typeof' each variable. Print one item per line.
-
+console.log(typeof shuttle)
+console.log(typeof speedMph)
+console.log(typeof dMars)
+console.log(typeof dMoon)
+console.log(typeof mpk)
 // Calculate a space mission below
-
+let milesToMars = dMars * mpk
+let hoursToMars = milesToMars / speedMph
+let daysToMars = hoursToMars / 24
 // Print the results of the space mission calculations below
-
+console.log(shuttle + " will take " + daysToMars + " days to reach Mars. ")
 // Calculate a trip to the moon below
-
+let milesToMoon = dMoon * mpk
+let hoursToMoon = milesToMoon / speedMph
+let daysToMoon = hoursToMoon / 24
 // Print the results of the trip to the moon below
+console.log(shuttle + " will take " + daysToMoon + " days to reach Mars. ")
