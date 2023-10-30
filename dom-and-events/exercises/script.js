b36341609174a61e28095ea9d7dd5cc359afc612
ngarner98 <exmachina091098@gmail.com>
2023-10-30 17:21:23 -0500

    DOM exercise

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,7 +4,23 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
-    
+    button.addEventListener('click', event => {
+        paragraph.innerHTML = 'Houston! We have liftoff!';
+     });
+    missionAbort.addEventListener('mouseover', event => {
+        missionAbort.style.backgroundColor = 'red'
+    });
+    missionAbort.addEventListener('mouseleave', event => {
+        missionAbort.style.backgroundColor = ''
+    });
+    missionAbort.addEventListener('click', event => {
+        let prompt = 'Are you sure you want to abort the mission?'
+        confirm(prompt)
+        if(prompt) {
+            paragraph.innerHTML = 'Mission aborted! Space shuttle returning home'
+        }
+    });
+
 }
 
 window.addEventListener("load", init);
