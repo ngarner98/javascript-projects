fbc901f40341834e8622d5ae5acd0fae531cd9ec
ngarner98 <exmachina091098@gmail.com>
2023-11-06 22:12:10 -0600

    Studio

--- fetch/studio/script.js
+++ fetch/studio/script.js
@@ -1,1 +1,28 @@
 //TODO: Add Your Code Below
+window.addEventListener("load", function(){
+
+    const container = this.document.getElementById('container');
+        
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json")
+    .then(function(response) {
+        response.json().then(function (data) {
+
+            for (let i=0; i < data.length; i++) {
+                let astronaut = data[i];
+                container.innerHTML += `
+                <div class = "astronaut">
+                    <div class = "bio">
+                    <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
+                    <ul>
+                        <li>Hours in space: ${astronaut.hoursInSpace}</li>
+                        <li id= "statusCheck">Active: ${astronaut.active}</li>
+                        <li>Skills: ${astronaut.skills}</li>
+                    </ul>
+                    </div>
+                    <img class="avatar" src ="${astronaut.picture}">
+                </div>
+                `;  
+                }
+            });
+        });
+    });
